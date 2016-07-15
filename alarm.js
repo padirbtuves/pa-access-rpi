4db6ee90007409a151fd5232fda250be8b574007
vincnetas <vincnetas@gmail.com>
2016-07-15 21:19:26 +0300

    latest from device

--- alarm.js
+++ alarm.js
@@ -1,68 +1,68 @@
-var Gpio = require('tm-onoff').Gpio
+ar Gpio = require('onoff').Gpio
 
 var doorPin = new Gpio(3, 'in', 'falling')
 var sirenPin = new Gpio(4, 'out')
-var alarmSwitch = new Gpio(5, 'in', 'falling')
+var alarmSwitch = new Gpio(17, 'in', 'falling')
 
 var alarmActivated = false
 var alarmTriggered = false
 
-export = {
+module.exports = {
 
-	init: function() {
-		doorPin.watch(function(err, value) {
-			// if alarm is activated and door get opened, trigger alarm
-			if (alarmActivated) {
-				this.triggerAlarm()
-			}
-		})
+    init: function () {
+        doorPin.watch(function (err, value) {
+            console.log("Door pin " + value)
+                // if alarm is activated and door get opened, trigger alarm
+            if (alarmActivated) {
+                this.triggerAlarm()
+            }
+        })
 
-		alarmSwitch.watch(function(err, value) {
-			this.enableAlarm()
-		})
-	},
+        alarmSwitch.watch(function (err, value) {
+            this.enableAlarm()
+        })
+    },
 
-	tearDown: function() {
-		doorPin.unexport()
-		sirenPin.unexport()
-		alarmSwitch.unexport()
-	}
+    tearDown: function () {
+        doorPin.unexport()
+        sirenPin.unexport()
+        alarmSwitch.unexport()
+    },
 
-	triggerAlarm: function() {
-		if (!alarmTriggered) {
-			alarmTriggered = true
-			sirenPin.write(1)
-			this.notifyServer()
+    triggerAlarm: function () {
+        if (!alarmTriggered) {
+            alarmTriggered = true
+            sirenPin.write(1)
+            this.notifyServer()
 
-			// Keep siren for 30 minutes or until disabled
-			setTimeout(function() {
-				this.disableAlarm()
-			}, 60000 * 30)
-		}
-	},
+            // Keep siren for 30 minutes or until disabled
+            setTimeout(function () {
+                this.disableAlarm()
+            }, 60000 * 30)
+        }
+    },
 
-	notifyServer: function () {
-		var url = "http://mano.padirbtuves.lt/auth/alarm"
+    notifyServer: function () {
+        var url = "http://mano.padirbtuves.lt/auth/alarm"
 
-		request({
-		    url: url,
-		    json: true
-		}, function (error, response, body) {
-			// What to do with response?
-		})
-	}
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            // What to do with response?
+        })
+    },
 
-	enableAlarm: function() {
-		// when alarm switch is pressed, wait 10 seconds and activate alarm
-		setTimeout(function() {
-			alarmActivated = true
-		}, 10000)	
-	}
+    enableAlarm: function () {
+        // when alarm switch is pressed, wait 10 seconds and activate alarm
+        setTimeout(function () {
+            alarmActivated = true
+        }, 10000)
+    },
 
-	disableAlarm: function() {
-		alarmActivated = false
-		alarmTriggered = false
-		sirenPin.write(0)
-	}	
+    disableAlarm: function () {
+        alarmActivated = false
+        alarmTriggered = false
+        sirenPin.write(0)
+    },
 }
-
--- go.js
+++ go.js
@@ -1,11 +1,12 @@
 var request = require('request')
 var storage = require('node-persist')
-var Gpio = require('tm-onoff').Gpio
+var Gpio = require('onoff').Gpio
 var cache = require('js-cache')
 var nfc = require('nfc').nfc
 var alarm = require('./alarm')
 
-var lockPin = new Gpio(2, 'out')
+var lockPin = new Gpio(5, 'out')
+var soundPin = new Gpio(6, 'out')
 
 var lockTimeoutId = null
 
@@ -13,60 +14,69 @@ storage.init()
 alarm.init()
 
 function handleTag(tag) {
-	var tagInfo = storage.getItem(tag.id)
+    var tagInfo = storage.getItem(tag.id)
 
-	if (tagInfo != null) {
-		openDoor(tagInfo)
-	} 
+    if (tagInfo != null) {
+        openDoor(tagInfo)
+    }
 
-	getTagInfo(tag.id, openDoor)
+    getTagInfo(tag.id, openDoor)
 }
 
 function openDoor(tagInfo) {
-	if (tagInfo.valid && lockTimeoutId == null) {
-		alarm.disableAlarm()
+    if (tagInfo.valid && lockTimeoutId == null) {
+        alarm.disableAlarm()
 
-		lockPin.write(1)
-		lockTimeoutId = setTimeout(function() {
-			lockPin.write(0)
-			lockTimeoutId = null
-		}, 5000)
-	}
+        lockPin.write(1)
+        soundPin.write(1)
+        setTimeout(function () {
+            soundPin.write(0)
+        }, 10)
+
+        lockTimeoutId = setTimeout(function () {
+            lockPin.write(0)
+            lockTimeoutId = null
+        }, 5000)
+    }
 }
 
 function getTagInfo(tagId, callback) {
-	var url = "http://mano.padirbtuves.lt/auth/nfc?id=" + tagId
+    var url = "http://mano.padirbtuves.lt/auth/nfc?id=" + tagId
 
-	request({
-	    url: url,
-	    json: true
-	}, function (error, response, tagInfo) {
-	    if (!error && response.statusCode === 200 && tagId.valid) {
-	        storage.setItem(tagId, tagInfo)
-	        callback(tagInfo)
-	    } else {
-	    	storage.removeItem(tagId)
-	    }
-	})
+    request({
+        url: url,
+        json: true
+    }, function (error, response, tagInfo) {
+        console.log("Tag : " + tagInfo.id)
+        if (!error && response.statusCode === 200 && tagInfo.valid) {
+            storage.setItem(tagId, tagInfo)
+            callback(tagInfo)
+        } else {
+            storage.removeItem(tagId)
+        }
+    })
 }
 
 function convertTagId(tagId) {
-    return parseInt(tagId.split(":").reverse().join(''), 16)
+    return parseInt(tagId.split(":").reverse().join(''), 16).toString()
 }
 
-new nfc.NFC().on('read', function(tag) {
-	tag.id = convertTagId(tag.uid)
+new nfc.NFC().on('read', function (tag) {
+    tag.id = convertTagId(tag.uid)
 
-	var cachedTag = cache.get(tag.uid)
-	if (cachedTag == null) {
-		handleTag(tag)
-	}
+    var cachedTag = cache.get(tag.uid)
+    if (cachedTag == null) {
+        handleTag(tag)
+    }
 
-	cache.set(tag.uid, tag, 1000)
+    cache.set(tag.uid, tag, 1000)
 }).start()
 
 function exit() {
-	lockPin.unexport()
+    lockPin.unexport()
+    soundPin.unexport()
 }
 
-process.on('SIGINT', exit)
+console.log("Started")
+
+//process.on('SIGINT', exit)
